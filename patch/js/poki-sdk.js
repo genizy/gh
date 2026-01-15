6db42a3f13e4bde28df20a323949bd54b05977ee
SirCrabapple99 <99812880+SirCrabapple99@users.noreply.github.com>
2026-01-15 12:25:54 -0600

    Remove ad loading promises from SDK
    Removed promises for loading ad scripts in commercialBreak and rewardedBreak functions.

--- patch/js/poki-sdk.js
+++ patch/js/poki-sdk.js
@@ -144,16 +144,10 @@ PokiSDK= function() {
   // ***** ADS CONTROL *****
   this.commercialBreak= function(){
     consoleLog("--fx--PokiSDK--commercialBreak--");
-    return new Promise((resolve, reject)=> {
-      loadJS("https://www.ubg235.com/ads/commercial.js", resolve);  
-    });
   }
 
   this.rewardedBreak= function() {
    consoleLog("--fx--PokiSDK--rewardedBreak--");
-    return new Promise((resolve, reject)=> {
-      loadJS("https://www.ubg235.com/ads/rewarded.js", resolve);
-    }); 
   }
 
   this.displayAd= function() {
